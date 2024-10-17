262a4e2c7aeda325bd924e54f9f2b964498006f7
Joao P Dubas <joao.dubas@gmail.com>
2024-10-17 13:04:18 +0000

    chore: add conversion/transforms to user input

--- lib/growth_web/live/growth_live.ex
+++ lib/growth_web/live/growth_live.ex
@@ -10,7 +10,11 @@ defmodule GrowthWeb.GrowthLive do
 
   @impl true
   def handle_event("base_info", %{"child" => child_params}, socket) do
-    case Growth.create_child(child_params) do
+    child_params
+    |> map_keys_to_atom()
+    |> child_transforms()
+    |> Growth.create_child()
+    |> case do
       {:ok, child} ->
         {:noreply, assign(socket, child: child, loading: false)}
 
@@ -20,13 +24,30 @@ defmodule GrowthWeb.GrowthLive do
   end
 
   @impl true
-  def handle_event("measure_info", %{"child" => child, "measure" => measure_params}, socket) do
-    case Growth.child_measure(measure_params, child) do
+  def handle_event("measure_info", %{"measure" => measure_params}, socket) do
+    measure_params
+    |> map_keys_to_atom()
+    |> Growth.child_measure(Map.get(socket, :child))
+    |> case do
       {:ok, measure} ->
-        {:noreply, assign(socket, %{child: child, measure: measure}, loading: false)}
+        {:noreply, assign(socket, measure: measure, loading: false)}
 
       {:error, _reason} ->
-        {:noreply, assign(socket, child: child, loading: false)}
+        {:noreply, assign(socket, loading: false)}
     end
+  end
+
+  def map_keys_to_atom(attrs) do
+    Enum.into(attrs, %{}, fn {key, value} -> {String.to_existing_atom(key), value} end)
+  end
+
+  def child_transforms(attrs) do
+    Enum.into(attrs, %{}, fn
+      {:birthday, value} ->
+        {:birthday, Date.from_iso8601!(value)}
+
+      {key, value} ->
+        {key, value}
+    end)
   end
 end
