613a2585de8091a63400e00a31caf96d4c449608
Joao P Dubas <joao.dubas@gmail.com>
2026-07-17 09:54:48 -0300

    chore: add documentation to `Result`

--- lib/growth/result.ex
+++ lib/growth/result.ex
@@ -1,5 +1,11 @@
 defmodule Growth.Result do
-  @moduledoc false
+  @moduledoc """
+  Represents a result for a given measurement of a child, such as weight, height, etc.
+
+  This holds the `zscore` and `percentile` wich allow one to compare how this measurement fits agagainst common patterns. As well as, have a `classification` that summarizes this comparison.
+
+  To help knowing if the result is available or not, the `available?` field is set, so any consumer is aware if this result is not available, due to missing measurement value or missing classification data.
+  """
   @type null_number() :: number() | nil
   @type null_non_neg_number() :: number() | nil
   @type t() :: %__MODULE__{
@@ -29,6 +35,9 @@ defmodule Growth.Result do
     :available?
   ]
 
+  @doc """
+  Create a new result based on the availability of data and the data itself.
+  """
   @spec new(boolean(), map()) :: t()
   def new(false, _) do
     %__MODULE__{available?: false}
