84e634f16c137f8f42cd3be5c8e22ea060b9347c
Joao P Dubas <joao.dubas@gmail.com>
2026-07-20 14:41:32 -0300

    chore: move implementation to own form modules
    Delegate all calls to the specific modules.

--- lib/growth_web/form/form.ex
+++ lib/growth_web/form/form.ex
@@ -6,35 +6,14 @@ defmodule GrowthWeb.Form do
   alias GrowthWeb.Form.Child
   alias GrowthWeb.Form.Measure
 
-  @spec child_schema() :: Zoi.schema()
-  def child_schema, do: Child.schema()
-
-  @spec measure_schema() :: Zoi.schema()
-  def measure_schema, do: Measure.schema()
-
-  @spec child_parse(map()) :: Zoi.Context.t()
-  def child_parse(attrs), do: Zoi.Form.parse(child_schema(), attrs)
-
-  @spec measure_parse(map()) :: Zoi.Context.t()
-  def measure_parse(attrs), do: Zoi.Form.parse(measure_schema(), attrs)
-
-  @spec child_form(map() | Zoi.Context.t(), atom()) :: Phoenix.HTML.Form.t()
-  @spec child_form(map() | Zoi.Context.t(), atom(), keyword()) :: Phoenix.HTML.Form.t()
-  def child_form(attrs, form_name, opts \\ [])
-
-  def child_form(%Zoi.Context{} = attrs, form_name, opts),
-    do: Phoenix.Component.to_form(attrs, Keyword.put(opts, :as, form_name))
-
-  def child_form(attrs, form_name, opts),
-    do: attrs |> child_parse() |> child_form(form_name, opts)
-
-  @spec measure_form(map() | Zoi.Context.t(), atom()) :: Phoenix.HTML.Form.t()
-  @spec measure_form(map() | Zoi.Context.t(), atom(), keyword()) :: Phoenix.HTML.Form.t()
-  def measure_form(attrs, form_name, opts \\ [])
-
-  def measure_form(%Zoi.Context{} = attrs, form_name, opts),
-    do: Phoenix.Component.to_form(attrs, Keyword.put(opts, :as, form_name))
-
-  def measure_form(attrs, form_name, opts),
-    do: attrs |> measure_parse() |> measure_form(form_name, opts)
+  defdelegate child_schema, to: Child, as: :schema
+  defdelegate child_parse(attrs), to: Child, as: :parse
+  defdelegate child_form(attrs), to: Child, as: :form
+  defdelegate child_form(attrs, form_name), to: Child, as: :form
+  defdelegate child_form(attrs, form_name, opts), to: Child, as: :form
+  defdelegate measure_schema, to: Measure, as: :schema
+  defdelegate measure_parse(attrs), to: Measure, as: :parse
+  defdelegate measure_form(attrs), to: Measure, as: :form
+  defdelegate measure_form(attrs, form_name), to: Measure, as: :form
+  defdelegate measure_form(attrs, form_name, opts), to: Measure, as: :form
 end
--- lib/growth_web/form/measure.ex
+++ lib/growth_web/form/measure.ex
@@ -13,6 +13,26 @@ defmodule GrowthWeb.Form.Measure do
             coerce: true
           )
 
+  @spec form(map() | Zoi.Context.t()) :: Phoenix.HTML.Form.t()
+  @spec form(map() | Zoi.Context.t(), atom()) :: Phoenix.HTML.Form.t()
+  @spec form(map() | Zoi.Context.t(), atom(), keyword()) :: Phoenix.HTML.Form.t()
+  def form(attrs, form_name \\ :measure, opts \\ [])
+
+  def form(%Zoi.Context{} = attrs, form_name, opts) do
+    Phoenix.Component.to_form(attrs, Keyword.put(opts, :as, form_name))
+  end
+
+  def form(attrs, form_name, opts) do
+    attrs
+    |> parse()
+    |> form(form_name, opts)
+  end
+
+  @spec parse(map()) :: Zoi.Context.t()
+  def parse(attrs) do
+    Zoi.Form.parse(schema(), attrs)
+  end
+
   @spec schema :: Zoi.schema()
   def schema do
     Zoi.transform(@schema, fn
